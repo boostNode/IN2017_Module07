2e85bcb7073ab5b2ba7aa6c54874a20194aae778
boostNode <support@boostnode.com>
2017-03-07 20:13:34 -0600

    original

---
+++ WPF_Events/WPF_Events/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿/*
+ *  Author: Troy Davis
+ *  Project: Module07 - Understanding Events and Event-Driven Programming - WPF Form Example - GUI
+ *  Class: IN 2017 (Advanced C#)
+ *  Date: Mar 07, 2017 
+ *  Revision: Original
+ */
+
+using System.Windows;
+
+namespace WPF_Events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.Click += button_MyOtherClick;
+        }
+
+        private void button_MyOtherClick(object sender, RoutedEventArgs e)
+        {
+            myOtherLabel.Content = "Hello again from myOther WPF Label!";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            myLabel.Content = "Hello WPF World!";
+        }
+    }
+}
---
+++ WinForm_Events/WinForm_Events/Form1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *  Author: Troy Davis
+ *  Project: Module07 - Understanding Events and Event-Driven Programming - WinForm Example - GUI
+ *  Class: IN 2017 (Advanced C#)
+ *  Date: Mar 07, 2017 
+ *  Revision: Original
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace WinForm_Events
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_MyOtherClick;
+        }
+
+        private void button1_MyOtherClick(object sender, EventArgs e)
+        {
+            myOtherLabel.Text = "Hello again from myOther WinForm Label!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myLabel.Text = "Hello WinForm World!";
+        }
+    }
+}
